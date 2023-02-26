0b1a1f5a88d08a89886aa6872871d3fb32f16a0f
metaAI123 <113107100+metaAI123@users.noreply.github.com>
2023-02-26 18:33:54 +0800

    Add files via upload

---
+++ nvim/lua/init-packer.lua
@@ -0,0 +1,51 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  --括号自动补全
+  use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  --代码自动补全
+  use {
+    "hrsh7th/nvim-cmp",
+    -- commit = "4f1358e659d51c69055ac935e618b684cf4f1429",
+  } -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "neovim/nvim-lspconfig"
+  use "hrsh7th/cmp-nvim-lsp"
+  --主题配置
+  use 'folke/tokyonight.nvim'
+  --显示缩进层级
+  use "lukas-reineke/indent-blankline.nvim"
+  --颜色直接显示
+  use 'NvChad/nvim-colorizer.lua'
+    require("colorizer").setup {
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = true, -- "Name" codes like Blue or blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        AARRGGBB = false, -- 0xAARRGGBB hex codes
+        rgb_fn = false, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = "background", -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = false, -- Enable tailwind colors
+        -- parsers can contain values used in |user_default_options|
+        sass = { enable = false, parsers = { css }, }, -- Enable sass colors
+        virtualtext = "■",
+      },
+      -- all the sub-options of filetypes apply to buftypes
+      buftypes = {},
+  }
+  end)
+  
+
