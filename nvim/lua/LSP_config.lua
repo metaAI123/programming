ad1278a528765ddecfc4ec50cd8453951295d167
metaAI123 <113107100+metaAI123@users.noreply.github.com>
2023-02-26 18:32:56 +0800

    Delete nvim directory

--- nvim/lua/LSP_config.lua
+++
@@ -1,9 +0,0 @@
-require("mason").setup()
-local servers = { 'clangd',  'pyright', 'sumneko_lua',  }
-
--- Ensure the servers above are installed
-require('mason-lspconfig').setup {
-  ensure_installed = servers,
-}
-
-
--- nvim/lua/keymap.lua
+++
@@ -1,9 +0,0 @@
-local map = vim.api.nvim_set_keymap
-local opt = {noremap = true, silent = true }
-
---map("n", "<q-q>", "<Esc>", opt)
---map("v", "<q-q>", "<Esc>", opt)
---map("i", "<q-q>", "<Esc>", opt)
-map('n', '<A-m>', ':NvimTreeToggle<CR>', opt)
-map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
-map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
